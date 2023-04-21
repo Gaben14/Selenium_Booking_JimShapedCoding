52a93d5fae02ad73fd88275e45cc95c65b647a58
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2023-04-21 13:59:22 +0200

    The main test case are complete. The application is able to set currency, places, dates, person numbers and trigger the search.

--- booking/booking.py
+++ booking/booking.py
@@ -20,21 +20,19 @@ class Booking(webdriver.Chrome):
         self.implicitly_wait(25)
         self.maximize_window()
 
+    # Each method is different test case.
     def land_first_page(self):
         self.get(const.BASE_URL)
 
     def change_currency(self, currency):
         currency_element = self.find_element(By.CSS_SELECTOR, 'button[data-testid="header-currency-picker-trigger"]')
         currency_element.click()
-        # time.sleep(5)
 
         selected_currency_element = self.find_element(By.XPATH,
                                                       f'//button[@data-testid="selection-item"] //div[text()="{currency}"]')
         # The *[] means all items should be checked
         # If you need to check only specific HTML tags - use it like this: div[]
         selected_currency_element.click()
-
-        # time.sleep(5)
 
     def select_place_to_go(self, place_to_go):
         search_field = self.find_element(By.CSS_SELECTOR, 'input[name="ss"')
@@ -45,7 +43,43 @@ class Booking(webdriver.Chrome):
         first_result = self.find_element(By.CSS_SELECTOR, 'ul[data-testid="autocomplete-results"] > li:nth-child(1)')
         first_result.click()
 
-        time.sleep(5)
+    def select_dates(self, check_in_date, check_out_date):
+        check_in_element = self.find_element(By.CSS_SELECTOR, f'span[data-date="{check_in_date}"]')
+        check_in_element.click()
+
+        check_out_element = self.find_element(By.CSS_SELECTOR, f'span[data-date="{check_out_date}"]')
+        check_out_element.click()
+
+    def select_adults(self, count=1):
+        # 1. Click on the button so that adult config is visible.
+        selection_element = self.find_element(By.CSS_SELECTOR, 'button[data-testid="occupancy-config"]')
+        selection_element.click()
+
+        # 2. Decrease the Adults count to the minimum (1) on the page
+        # We need to get the current value, and decrease it until it reaches 1.
+        while True:
+            decrease_adults_element = self.find_element(By.XPATH,
+                                                        '//div[@data-testid="occupancy-popup"]/div/div/div[2]/button[1]')
+            decrease_adults_element.click()
+            # If the value of adults == 1, then the loop should stop.
+            adults_value_element = self.find_element(By.XPATH,
+                                                     '//div[@data-testid="occupancy-popup"]/div/div/div[2]/span')
+            adults_value = int(adults_value_element.text)
+
+            if adults_value == 1:
+                break
+
+        # 3. Locate the increase button and increase the amount of users to a specific number.
+        # The loop should run until it's specified in the method / function call.
+        increase_button_element = self.find_element(By.XPATH,
+                                                    '//div[@data-testid="occupancy-popup"]/div/div/div[2]/button[2]')
+        # Because the default value at booking.com is 1, we need to subtract 1 from the count variable.
+        for _ in range(count - 1):
+            increase_button_element.click()
+
+    def click_search(self):
+        search_btn = self.find_element(By.CSS_SELECTOR, 'button[type="submit"')
+        search_btn.click()
 
     # Teardown
     def __exit__(self, exc_type, exc_val, exc_tb):
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ with Booking() as bot:
     bot.land_first_page()
     bot.change_currency("USD")
     bot.select_place_to_go("New York")
-
-
+    bot.select_dates("2023-05-21", "2023-05-24")  # Date format: 2023-05-21
+    bot.select_adults(10)
+    bot.click_search()
 
